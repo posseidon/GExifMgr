2af159d410eff93168a0128c59bc6e34345561ee
Binh Nguyen <posseidon@gmail.com>
2023-04-14 14:15:04 +0200

    Fix: test files copied to output folder.

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -13,10 +13,7 @@ namespace UnitTest
         public void TestMetadataExtractor()
         {
             IExifReader reader = ExifReaderFactory.GetReader(ReaderType.MetadataExtractor);
-            // List<string> tags = reader.Read("c:\\Users\\ngtha\\Downloads\\Drone_Images\\DSC00153.JPG");
-            // List<string> tags = reader.Read("c:\\Users\\ngtha\\Downloads\\Gregg1_2\\DJI_0001.JPG");
-            // List<string> tags = reader.Read("c:\\Users\\ngtha\\Downloads\\DroneMapper_Golf9\\Ortho-DroneMapper.tif");
-            List<string> tags = reader.Read("c:\\Users\\ngtha\\Downloads\\DroneMapper_Golf9\\DEM32-DroneMapper.tif");
+            List<string> tags = reader.Read("Resources/Landsat_ETM_2001-08-26_multispectral.tif");
             Assert.IsNotEmpty(tags);
             foreach (var tag in tags)
             {
@@ -28,10 +25,7 @@ namespace UnitTest
         public void TestExifLib()
         {
             IExifReader reader = ExifReaderFactory.GetReader(ReaderType.ExifLib);
-            // List<string> tags = reader.Read("c:\\Users\\ngtha\\Downloads\\Drone_Images\\DSC00153.JPG");
-            // List<string> tags = reader.Read("c:\\Users\\ngtha\\Downloads\\Gregg1_2\\DJI_0001.JPG");
-            // List<string> tags = reader.Read("c:\\Users\\ngtha\\Downloads\\DroneMapper_Golf9\\Ortho-DroneMapper.tif");
-            List<string> tags = reader.Read("c:\\Users\\ngtha\\Downloads\\DroneMapper_Golf9\\DEM32-DroneMapper.tif");
+            List<string> tags = reader.Read("Resources/Landsat_ETM_2001-08-26_multispectral.tif");
             Assert.IsNotEmpty(tags);
             foreach (var tag in tags)
             {
