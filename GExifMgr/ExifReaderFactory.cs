34668682a43767440c0f17290632f8de26a1b0bb
Binh Nguyen <posseidon@gmail.com>
2023-04-14 21:39:44 +0200

    Cleanup code

--- GExifMgr/ExifReaderFactory.cs
+++ GExifMgr/ExifReaderFactory.cs
@@ -10,7 +10,7 @@
 
         public static IExifReader GetReader(ReaderType Type)
         {
-            return ExifReaders.GetValueOrDefault(Type);
+            return ExifReaders[Type];
         }
     }
 }
